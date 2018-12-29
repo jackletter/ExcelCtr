d97bd96e084c277bb4c0b0a4703d7ea51af7ad94
jackletter <1286317554@qq.com>
2018-12-29 14:19:09 +0800

    '修改版本号'

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // 特性集控制。更改这些特性值可修改
 // 与程序集关联的信息。
 [assembly: AssemblyTitle("ExcelCtr")]
-[assembly: AssemblyDescription("Excel操作工具-胡庆杰")]
+[assembly: AssemblyDescription("Excel操作工具")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("JackOA")]
+[assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("ExcelCtr")]
-[assembly: AssemblyCopyright("Copyright © JackOA2016")]
+[assembly: AssemblyCopyright("")]
 [assembly: AssemblyTrademark("无商标")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // 可以指定所有这些值，也可以使用“生成号”和“修订号”的默认值，
 // 方法是按如下所示使用“*”: 
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.0.1")]
+[assembly: AssemblyFileVersion("1.0.0.1")]
